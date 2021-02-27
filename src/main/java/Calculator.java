b136d5de9b2de42c9177103ea31ceb8438a7bcb6
Balashoffs <vladbalashov@vlads-imac.lan>
2021-02-26 18:47:39 -0700

    Added more comments

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -45,7 +45,7 @@ class Calculator {
         if (n > 0) { // check if n > 0
             arr[1] = 1;
         }
-        for (int i = 2; i < n + 1; i++) {
+        for (int i = 2; i < n + 1; i++) { // gor n > 2
             arr[i] = arr[i-1] + arr[i-2];
         }
         return arr[n];
