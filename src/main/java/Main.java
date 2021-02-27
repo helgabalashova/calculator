175c6527b8983a1fb3701fb1d51f404ba9af6500
Balashoffs <vladbalashov@vlads-imac.lan>
2021-02-26 20:47:54 -0700

    Added while loop to scan more commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,27 +6,31 @@ public class Main {
         System.out.println("Enter a command:");
         Scanner scanner = new Scanner(System.in);
         String command = scanner.next();
-        if  (command.equals("add")) {
-            int a = scanner.nextInt();
-            int b  = scanner.nextInt();
-            int res = calc.add(a, b);
-            System.out.println(res);
+        while (!command.equals("q")) {
+            if  (command.equals("add")) {
+                int a = scanner.nextInt();
+                int b  = scanner.nextInt();
+                int res = calc.add(a, b);
+                System.out.println(res);
+            }
+            else if (command.equals("subtract")) {
+                int a = scanner.nextInt();
+                int b  = scanner.nextInt();
+                int res = calc.subtract(a, b);
+                System.out.println(res);
+            }
+            else if (command.equals("fibonacciNumberFinder")) {
+                int a = scanner.nextInt();
+                int res = calc.fibonacciNumberFinder(a);
+                System.out.println(res);
+            }
+            else if (command.equals("intToBinaryNumber")) {
+                int a = scanner.nextInt();
+                String res = calc.intToBinaryNumber(a);
+                System.out.println(res);
+            }
+            command = scanner.next();
         }
-        else if (command.equals("subtract")) {
-            int a = scanner.nextInt();
-            int b  = scanner.nextInt();
-            int res = calc.subtract(a, b);
-            System.out.println(res);
-        }
-        else if (command.equals("fibonacciNumberFinder")) {
-            int a = scanner.nextInt();
-            int res = calc.fibonacciNumberFinder(a);
-            System.out.println(res);
-        }
-        else if (command.equals("intToBinaryNumber")) {
-            int a = scanner.nextInt();
-            String res = calc.intToBinaryNumber(a);
-            System.out.println(res);
-        }
+        scanner.close();
     }
 }
